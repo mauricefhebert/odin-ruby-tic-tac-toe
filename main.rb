97f9ab39f57e0085b56c98ebb04bdf56c98f7a07
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-01 00:58:15 -0400

    Implimentation of the check_winner func need some tweaking

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@
 # Player class used to create the player and keep track of it's score
 class Player
   attr_accessor :name, :symbol, :score
+
   def initialize(name, symbol, score = 0)
     @name = name
     @symbol = symbol
@@ -14,8 +15,8 @@ $player2 = Player.new('Player 2', 'O')
 $current_player = $player1
 
 # Change the current player
-def change_current_player()
-  $current_player == $player1 ? $current_player = $player2 : $current_player = $player1
+def change_current_player
+  $current_player = $current_player == $player1 ? $player2 : $player1
 end
 
 # Board creation and display
@@ -29,46 +30,50 @@ def display_board(board)
 end
 display_board($board)
 
-def check_winner()
+def check_winner
   winner = false
 
   win_combo = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ]
 
-  if $board[0..2] == "X"
-    puts "winner"
+  win_combo.each do |wcn|
+    if $board[wcn] == $player1.symbol
+      puts "#{$player1.name} win!"
+      winner = true
+    elsif $board[wcn] == $player2.symbol
+      puts "#{$player2.name} win!"
+      winner = true
+    end
+    return if winner == true
   end
 end
 
-check_winner()
-
-def play_round()
+def play_round
   # Prompt the user for a selection
   print 'Please make a selection with the available number: '
   player_selection = gets
-  
+
   # Make sure the player selection is a valide one between 1 and 9
-  while (player_selection.to_i < 1 || player_selection.to_i > 9) do
-      print 'Please make a selection with the available number: '
-      player_selection = gets
+  while player_selection.to_i < 1 || player_selection.to_i > 9
+    print 'Please make a selection with the available number: '
+    player_selection = gets
   end
 
   # Execute the game change
-    if $board[player_selection.to_i - 1] != $player1.symbol || $board[player_selection.to_i - 1] != $player2.symbol
-      $board[player_selection.to_i - 1] = $current_player.symbol
-      check_winner()
-      change_current_player()
-      display_board($board)
-      play_round()
-    end
+  if $board[player_selection.to_i - 1] != $player1.symbol || $board[player_selection.to_i - 1] != $player2.symbol
+    $board[player_selection.to_i - 1] = $current_player.symbol
+    check_winner
+    change_current_player
+    display_board($board)
+    play_round
   end
-play_round()
-
+end
+play_round
