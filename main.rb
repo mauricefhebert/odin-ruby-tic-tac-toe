f4ac4fa5f2d03c6bfa28fc7c7f60c5f5ac1ce455
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-01 16:11:13 -0400

    Add the function to prevent user to play if mark is already used

--- main.rb
+++ main.rb
@@ -1,97 +1,64 @@
 # frozen_string_literal: true
 
-# Player class used to create the player and keep track of it's score
+# 1.Class Player
+#   -> Allow the creation of player
 class Player
-  attr_accessor :name, :symbol, :score
+  attr_accessor :name, :mark
 
-  def initialize(name, symbol, score = 0)
+  def initialize(name, mark)
     @name = name
-    @symbol = symbol
-    @score = score
-  end
-end
-$player1 = Player.new('Player 1', 'X')
-$player2 = Player.new('Player 2', 'O')
-$current_player = $player1
-
-# Change the current player
-def change_current_player
-  $current_player = $current_player == $player1 ? $player2 : $player1
-end
-
-# Board creation and display
-$board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts '---------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts '---------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
-end
-display_board($board)
-
-def check_winner
-  winner = false
-
-  return puts 'The game is a tie!' if $board.none?(1..10) && winner == false
-
-  win_combo = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  x = 0
-  y = 0
-  win_combo.map do |wcn|
-    wcn.each do |b|
-      if $board[b] == $player1.symbol
-        x += 1
-      elsif $board[b] == $player2.symbol
-        y += 1
-      else
-        next
-      end
-    end
-    if x == 3
-      puts 'player 1 win'
-      winner = true
-      return winner
-    elsif y == 3
-      puts 'player 2 win'
-      winner = true
-      return winner
-    else
-      x = 0
-      y = 0
-    end
+    @mark = mark
   end
 end
 
-def play_round
+# Initialize the player and assign player 1 as the first player
+player_1 = Player.new('Player 1', 'X')
+player_2 = Player.new('Player 2', 'O')
+current_player = player_1
+
+# 2.Class Board
+#   -> Create the board
+#   -> Display board
+class Board
+  attr_accessor :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  # Display the board
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '---------'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '---------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+end
+# Initialize the Game board and display it
+board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board = Board.new(board_array)
+board.display_board
+
+# 3. Game function
+define_method('play') do
   # Prompt the user for a selection
-  # return 'Play again? [y/n]' if check_winner == true
-
   print 'Please make a selection with the available number: '
-  player_selection = gets
+  selection = gets.chomp
 
   # Make sure the player selection is a valide one between 1 and 9
-  while player_selection.to_i < 1 || player_selection.to_i > 9
+  while selection.to_i < 1 || selection.to_i > 9
     print 'Please make a selection with the available number: '
-    player_selection = gets
+    selection = gets.chomp
   end
 
-  # Execute the game change
-  if $board[player_selection.to_i - 1] != $player1.symbol || $board[player_selection.to_i - 1] != $player2.symbol
-    $board[player_selection.to_i - 1] = $current_player.symbol
-    check_winner
-    change_current_player
-    display_board($board)
-    play_round
+  # If user selection on the board is already filled return
+  unless board.board[selection.to_i - 1] == player_1.mark || board.board[selection.to_i - 1] == player_2.mark
+    board.board[selection.to_i - 1] = current_player.mark
+    # Check winner method will go here
+    current_player == player_1 ? (current_player = player_2) : (current_player = player_1)
+    board.display_board
   end
+  play
 end
-play_round
+play
