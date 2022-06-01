bf6da4bd562d3deaea9d4221081ab59fce266809
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-01 00:10:54 -0400

    Update almost done

--- main.rb
+++ main.rb
@@ -3,28 +3,23 @@
 # Player class used to create the player and keep track of it's score
 class Player
   attr_accessor :name, :symbol, :score
-
   def initialize(name, symbol, score = 0)
     @name = name
     @symbol = symbol
     @score = score
   end
 end
+$player1 = Player.new('Player 1', 'X')
+$player2 = Player.new('Player 2', 'O')
+$current_player = $player1
 
-module PlayerModule
-  player1 = Player.new('Player 1', 'X')
-  player2 = Player.new('Player 2', 'O')
-  current_user = player1
-
-  def change_player()
-    current_user == player2 ? current_user = player1 : current_user = player2
-  end
+# Change the current player
+def change_current_player()
+  $current_player == $player1 ? $current_player = $player2 : $current_player = $player1
 end
 
-
-
 # Board creation and display
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+$board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts '---------'
@@ -32,20 +27,48 @@ def display_board(board)
   puts '---------'
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
-display_board(board)
+display_board($board)
 
-def play_round()
-  include PlayerModule
-  print 'Please make a selection with the available number: '
-  player_selection = gets
-  display_board(board)
-  change_player()
-  board.each do |square|
-    if square != 'X' || square != 'O'
-      board[player_selection - 1] = current_user.symbol
-      display_board(board)
-    end
+def check_winner()
+  winner = false
+
+  win_combo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  if $board[0..2] == "X"
+    puts "winner"
   end
 end
 
+check_winner()
+
+def play_round()
+  # Prompt the user for a selection
+  print 'Please make a selection with the available number: '
+  player_selection = gets
+  
+  # Make sure the player selection is a valide one between 1 and 9
+  while (player_selection.to_i < 1 || player_selection.to_i > 9) do
+      print 'Please make a selection with the available number: '
+      player_selection = gets
+  end
+
+  # Execute the game change
+    if $board[player_selection.to_i - 1] != $player1.symbol || $board[player_selection.to_i - 1] != $player2.symbol
+      $board[player_selection.to_i - 1] = $current_player.symbol
+      check_winner()
+      change_current_player()
+      display_board($board)
+      play_round()
+    end
+  end
 play_round()
+
