82147dbf8470a98ae943f818c3bf8b8c99651cb4
mauricefhebert <filiatreaultmaurice@gmail.com>
2022-06-01 17:09:06 -0400

    Add the validation for a tie game

--- main.rb
+++ main.rb
@@ -40,7 +40,42 @@ board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 board = Board.new(board_array)
 board.display_board
 
-# 3. Game function
+# 3. Check Winner function
+define_method('check_winner') do
+  winner = false
+  win_combination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  x = 0
+  y = 0
+  win_combination.each do |i|
+    i.each do |j|
+      if board.board[j] == player_1.mark
+        x += 1
+        winner = true if x == 3
+      elsif board.board[j] == player_2.mark
+        y += 1
+        winner = true if y == 3
+      else
+        next
+      end
+    end
+    x = 0
+    y = 0
+  end
+  puts "It's a tie!" if winner == false && board.board.all? { |x| !x.is_a?(Integer) }
+  puts "#{current_player.name} win!" if winner == true
+end
+
+# 4. Game function
 define_method('play') do
   # Prompt the user for a selection
   print 'Please make a selection with the available number: '
@@ -55,7 +90,7 @@ define_method('play') do
   # If user selection on the board is already filled return
   unless board.board[selection.to_i - 1] == player_1.mark || board.board[selection.to_i - 1] == player_2.mark
     board.board[selection.to_i - 1] = current_player.mark
-    # Check winner method will go here
+    check_winner
     current_player == player_1 ? (current_player = player_2) : (current_player = player_1)
     board.display_board
   end
